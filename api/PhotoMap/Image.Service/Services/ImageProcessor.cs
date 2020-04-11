6e3fc9a495fc8151d2ecd4e3224dfba8076cf639
Vitaly Dudarau <vitalydudarev@gmail.com>
2020-04-11 13:42:57 +0300

    Fixed image rotation and cropping.

--- api/PhotoMap/Image.Service/Services/ImageProcessor.cs
+++ api/PhotoMap/Image.Service/Services/ImageProcessor.cs
@@ -8,135 +8,135 @@ namespace Image.Service.Services
     {
         private const int Quality = 100;
         private readonly Stream _stream;
+        private readonly bool _disposeStream;
 
-        public ImageProcessor(string fileName)
+        private SKBitmap _bitmap;
+        private SKImage _image;
+        private readonly SKCodec _codec;
+
+        public ImageProcessor(string filePath) : this(File.ReadAllBytes(filePath))
         {
-            var bytes = File.ReadAllBytes(fileName);
-            _stream = new MemoryStream(bytes);
         }
         
-        public ImageProcessor(byte[] bytes)
+        public ImageProcessor(byte[] bytes) : this(new MemoryStream(bytes))
         {
-            _stream = new MemoryStream(bytes);
+            _disposeStream = true;
         }
         
         public ImageProcessor(Stream stream)
         {
             _stream = stream;
+            _codec = SKCodec.Create(stream);
+            _bitmap = SKBitmap.Decode(_codec);
         }
         
-        public byte[] Resize(int size)
+        public void Crop(int size)
         {
-            using (var bitmap = SKBitmap.Decode(_stream))
+            int width, height;
+            if (_bitmap.Width > _bitmap.Height)
             {
-                int width, height;
-                if (bitmap.Width > bitmap.Height)
+                height = size;
+                width = _bitmap.Width * size / _bitmap.Height;
+            }
+            else
+            {
+                width = size;
+                height = _bitmap.Height * size / _bitmap.Width;
+            }
+
+            using (var resizedBitmap = _bitmap.Resize(new SKImageInfo(width, height), SKFilterQuality.High))
+            {
+                int x = (width - size) / 2;
+                int y = (height - size) / 2;
+
+                using (var image = SKImage.FromBitmap(resizedBitmap))
                 {
-                    height = size;
-                    width = bitmap.Width * size / bitmap.Height;
-                }
-                else
-                {
-                    width = size;
-                    height = bitmap.Height * size / bitmap.Width;
-                }
-
-                using (var resizedBitmap = bitmap.Resize(new SKImageInfo(width, height), SKFilterQuality.High))
-                {
-                    if (resizedBitmap == null)
-                        return null;
-
-                    int x = (width - size) / 2;
-                    int y = (height - size) / 2;
-
-                    using (var image = SKImage.FromBitmap(resizedBitmap))
-                    {
-                        var croppedImage = image.Subset(SKRectI.Create(x, y, height, height));
-                        var encodedData = croppedImage.Encode(SKEncodedImageFormat.Jpeg, Quality);
-
-                        return encodedData.ToArray();
-                    }
+                    _image = image.Subset(SKRectI.Create(x, y, size, size));
+                    _bitmap = SKBitmap.FromImage(_image);
                 }
             }
         }
 
-        public byte[] Rotate()
+        public void Rotate()
         {
-            using (var bitmap = RotateBitmap(_stream))
-            using (var image = SKImage.FromBitmap(bitmap))
-            {
-                var encodedData = image.Encode(SKEncodedImageFormat.Jpeg, Quality);
-
-                return encodedData.ToArray();
-            }
+            _bitmap = RotateBitmap();
+            _image = SKImage.FromBitmap(_bitmap).Subset(SKRectI.Create(0, 0, _bitmap.Width, _bitmap.Height));
         }
 
-        private static SKBitmap RotateBitmap(Stream stream)
+        public byte[] GetImageBytes()
         {
-            using (var codec = SKCodec.Create(stream))
-            {
-                var orientation = codec.EncodedOrigin;
-                var bitmap = SKBitmap.Decode(codec);
+            var encodedData = _image.Encode(SKEncodedImageFormat.Jpeg, Quality);
 
-                int width, height;
-                float dx, dy;
-                float degrees;
-                
-                switch (orientation)
-                {
-                    case SKEncodedOrigin.BottomRight:
-                    {
-                        width = bitmap.Width;
-                        height = bitmap.Height;
-                        dx = bitmap.Width;
-                        dy = bitmap.Height;
-                        degrees = 180;
-                        
-                        break;
-                    }
-
-                    case SKEncodedOrigin.RightTop:
-                    {
-                        width = bitmap.Height;
-                        height = bitmap.Width;
-                        dx = bitmap.Height;
-                        dy = 0;
-                        degrees = 90;
-                        
-                        break;
-                    }
-
-                    case SKEncodedOrigin.LeftBottom:
-                    {
-                        width = bitmap.Height;
-                        height = bitmap.Width;
-                        dx = 0;
-                        dy = bitmap.Height;
-                        degrees = 270;
-                        
-                        break;
-                    }
-
-                    default:
-                        return bitmap;
-                }
-                
-                var rotated = new SKBitmap(width, height);
-
-                using (var canvas = new SKCanvas(rotated))
-                {
-                    canvas.Translate(dx, dy);
-                    canvas.RotateDegrees(degrees);
-                    canvas.DrawBitmap(bitmap, 0, 0);
-                }
-
-                return rotated;
-            }
+            return encodedData.ToArray();
         }
 
         public void Dispose()
         {
-            _stream?.Dispose();
+            _bitmap?.Dispose();
+            _codec?.Dispose();
+            _image?.Dispose();
+            
+            if (_disposeStream)
+                _stream?.Dispose();
+        }
+
+        private SKBitmap RotateBitmap()
+        {
+            var orientation = _codec.EncodedOrigin;
+
+            int width, height;
+            float dx, dy;
+            float degrees;
+            
+            switch (orientation)
+            {
+                case SKEncodedOrigin.BottomRight:
+                {
+                    width = _bitmap.Width;
+                    height = _bitmap.Height;
+                    dx = _bitmap.Width;
+                    dy = _bitmap.Height;
+                    degrees = 180;
+                    
+                    break;
+                }
+
+                case SKEncodedOrigin.RightTop:
+                {
+                    width = _bitmap.Height;
+                    height = _bitmap.Width;
+                    dx = _bitmap.Height;
+                    dy = 0;
+                    degrees = 90;
+                    
+                    break;
+                }
+
+                case SKEncodedOrigin.LeftBottom:
+                {
+                    width = _bitmap.Height;
+                    height = _bitmap.Width;
+                    dx = 0;
+                    dy = _bitmap.Height;
+                    degrees = 270;
+                    
+                    break;
+                }
+
+                default:
+                    return _bitmap;
+            }
+            
+            var rotated = new SKBitmap(width, height);
+
+            using (var canvas = new SKCanvas(rotated))
+            {
+                canvas.Translate(dx, dy);
+                canvas.RotateDegrees(degrees);
+                canvas.DrawBitmap(_bitmap, 0, 0);
+            }
+
+            return rotated;
         }
     }
 }
