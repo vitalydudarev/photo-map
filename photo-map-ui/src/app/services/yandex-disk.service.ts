5c5edfd8eba155b6b63c37e67c953076b74fe45c
Vitaly Dudarau <vitalydudarev@gmail.com>
2020-07-21 08:28:25 +0300

    Fixed getting Yandex.Disk authorized user.

--- photo-map-ui/src/app/services/yandex-disk.service.ts
+++ photo-map-ui/src/app/services/yandex-disk.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from "../../environments/environment";
+import { UserModel } from '../models/user.model';
 
 @Injectable()
 export class YandexDiskService {
@@ -12,6 +13,10 @@ export class YandexDiskService {
   }
 
   public addUser(id: number, name: string, accessToken: string, expiresIn: number): Observable<any> {
-    return this._httpClient.post<any>(`${this.url}`, { id: id, name: name, accessToken: accessToken, tokenExpiresIn: expiresIn });
+    return this._httpClient.post<any>(`${this.url}`, { id: id, name: name, yandexDiskAccessToken: accessToken, yandexDiskTokenExpiresIn: expiresIn });
+  }
+
+  public getUser(id: number): Observable<UserModel> {
+    return this._httpClient.get<UserModel>(`${this.url}/${id}`);
   }
 }
