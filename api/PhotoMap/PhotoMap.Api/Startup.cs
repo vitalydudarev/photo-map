1d18857d84064b61237643d45370537938a7917d
Vitaly Dudarau <vitalydudarev@gmail.com>
2020-07-27 08:58:40 +0300

    Use Swagger in PhotoMap.Api.

--- api/PhotoMap/PhotoMap.Api/Startup.cs
+++ api/PhotoMap/PhotoMap.Api/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using PhotoMap.Api.Database;
 using PhotoMap.Api.ServiceClients.StorageService;
 using PhotoMap.Api.Services;
@@ -38,6 +39,11 @@ namespace PhotoMap.Api
             services.AddScoped<IStorageService, StorageServiceClient>();
 
             services.AddDbContext<PhotoMapContext>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PhotoMap API V1", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,6 +65,13 @@ namespace PhotoMap.Api
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PhotoMap API V1");
+            });
         }
     }
 }
