0e80ac52235948710dc8dec5d416b4df82b1aa5d
Vitaly Dudarau <vitalydudarev@gmail.com>
2020-10-30 00:43:32 +0300

    Deleted Dropbox columns.

---
+++ api/PhotoMap/PhotoMap.Api/Migrations/20201029214003_DeletedDropboxColumns.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhotoMap.Api.Migrations
+{
+    public partial class DeletedDropboxColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DropboxStatus",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DropboxToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DropboxTokenExpiresOn",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DropboxStatus",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DropboxToken",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DropboxTokenExpiresOn",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+    }
+}
